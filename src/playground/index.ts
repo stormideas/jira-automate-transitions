be2dc039f914dc10e8d56eaf0a582f0d0d0cb578
Michal Dabrowski <michal.dabrowski@stormideas.com>
2023-06-15 16:02:39 +0200

    INIT

--- src/playground/index.ts
+++ src/playground/index.ts
@@ -1,38 +1,7 @@
-import { config } from "dotenv";
-config();
-import { handleTransitionIssue } from "../handlers";
-import { JiraClient } from "../jira";
-import { info } from "console";
-import { parseString } from "../get-args"
 
-const {
-  JIRA_API_ENDPOINT,
-  JIRA_ISSUE_KEY,
-  JIRA_AUTH_TOKEN,
-  JIRA_ACCOUNT,
-  JIRA_REVIEW_ID,
-  JIRA_IN_PROGRESS_ID
-} = process.env;
+import "console"
 
-const jiraToken = Buffer.from(`${JIRA_ACCOUNT}:${JIRA_AUTH_TOKEN}`).toString(
-  "base64"
-);
+const { GITHUB_TOKEN, JIRA_API_TOKEN } = process.env
 
-(async () => {
-  let x = 10, y = 10;
-
-  switch (x - y) {
-    case 0: {
-      console.log("Result: 0");
-      // break;
-    }
-    case 5:
-      console.log("Result: 5");
-      // break;
-    case 10:
-      console.log("Result: 10");
-      break;
-    default:
-      console.log('da')
-  }
-})();
+console.log(GITHUB_TOKEN)
+console.log(JIRA_API_TOKEN)
